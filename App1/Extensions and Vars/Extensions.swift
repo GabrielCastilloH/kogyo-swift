aab8823bc328c3429a2254297faa44528075efdc
gabrielc <gabocastillo2447@gmail.com>
2024-06-06 15:34:49 +0700

    Search bar working, going to fix cancel button.

---
+++ App1/Extensions and Vars/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/6/24.
+//
+
+import Foundation
---
+++ App1/Home Screen/SearchTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  searchTableCell.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/6/24.
+//
+
+import UIKit
+
+class searchTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
