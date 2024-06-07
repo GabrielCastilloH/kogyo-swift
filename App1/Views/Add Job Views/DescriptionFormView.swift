967245904a96e7ab3e4cdc6c8e15c92c9e6ad6ca
gabrielc <gabocastillo2447@gmail.com>
2024-06-07 14:56:03 +0700

    Finish kind section for creating a new job. Start description.

---
+++ App1/Views/Add Job Views/DescriptionFormView.swift
@@ -0,0 +1,20 @@
+//
+//  DescriptionFormView.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/7/24.
+//
+
+import UIKit
+
+class DescriptionFormView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
