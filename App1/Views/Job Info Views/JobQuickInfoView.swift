8f518a95bd220c97ad25565a252ee8149cadddf6
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-10 00:19:03 +0700

    Finish job info view (without database objects, without chat feature)

---
+++ App1/Views/Job Info Views/JobQuickInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  JobQuickInfoView.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/9/24.
+//
+
+import UIKit
+
+class JobQuickInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
