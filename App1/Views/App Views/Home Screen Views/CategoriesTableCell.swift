6a8b6a6a2754ef97568e18448d2251edc71f6a35
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-10 17:49:20 +0700

    Create table view double scroll functionality, but there is a navigation bar bug.

---
+++ App1/Views/App Views/Home Screen Views/CategoriesTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoriesTableCell.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/10/24.
+//
+
+import UIKit
+
+class CategoriesTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
