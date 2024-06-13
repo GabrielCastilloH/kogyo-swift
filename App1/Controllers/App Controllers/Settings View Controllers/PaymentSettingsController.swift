db36ed991af4611bdb8a9d071dcb10cef0dcb31f
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-13 19:41:57 -0400

    Notifications page created

---
+++ App1/Controllers/App Controllers/Settings View Controllers/PaymentSettingsController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentSettingsController.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/13/24.
+//
+
+import UIKit
+
+class PaymentSettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
