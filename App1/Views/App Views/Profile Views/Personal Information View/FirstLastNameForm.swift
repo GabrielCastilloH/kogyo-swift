e3ea96b26413d96d2beb294b86bee04a891f2b0a
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-13 19:09:18 -0400

    finish personal info screen.

---
+++ App1/Views/App Views/Profile Views/Personal Information View/FirstLastNameForm.swift
@@ -0,0 +1,20 @@
+//
+//  FirstLastNameForm.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/13/24.
+//
+
+import UIKit
+
+class FirstLastNameForm: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
