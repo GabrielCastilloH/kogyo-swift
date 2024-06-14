207f6d6ecf79d76f2cac52be9036558e40d3d3a8
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-14 16:21:06 -0400

    Finish doing database required before creating the listener.

---
+++ App1/Models & Other/Constants+Extensions/FirestoreHandler.swift
@@ -0,0 +1,8 @@
+//
+//  FirestoreHandler.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/14/24.
+//
+
+import Foundation
---
+++ App1/Models & Other/Models/Helper.swift
@@ -0,0 +1,8 @@
+//
+//  Helper.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/14/24.
+//
+
+import Foundation
