7c0fede726ce79accdcfc37f0152e1289f00ae46
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-17 21:23:51 -0500

    Fix lots of bugs and missing features. Prepare to add media.

---
+++ App1/Views/App Views/Add Job Views/AddMediaView/EmptyMediaView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyMediaView.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/17/24.
+//
+
+import UIKit
+
+class EmptyMediaView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
