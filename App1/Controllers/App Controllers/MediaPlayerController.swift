984b920a7e85d2fa17f2696373448ca09bd5bc28
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-22 14:10:52 -0500

    Allow user to see images and videos on job info screen.

---
+++ App1/Controllers/App Controllers/MediaPlayerController.swift
@@ -0,0 +1,29 @@
+//
+//  MediaPlayerController.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/22/24.
+//
+
+import UIKit
+
+class MediaPlayerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
