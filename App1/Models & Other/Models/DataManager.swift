a36b77edac6f345d93667251c448283e8b315839
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-22 14:37:34 -0500

    Right before completely restructuring the way data is loaded and fetched.

---
+++ App1/Models & Other/Models/DataManager.swift
@@ -0,0 +1,20 @@
+//
+//  DataManager.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/22/24.
+//
+
+import UIKit
+
+class DataManager: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
