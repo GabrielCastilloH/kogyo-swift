0477902da56a2267d3203104b4ae3bbe10b1935f
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-06-22 19:45:09 -0500

    Craete DataManager functionality to prevent having to reload data but helper profile image not loaded for some reason (probably the way  my code is written.)

---
+++ App1/Models & Other/Data/DataManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataManager.swift
+//  App1
+//
+//  Created by Gabriel Castillo on 6/22/24.
+//
+
+import UIKit
+
+class DataManager {
+    static var currentJobs: [String: Job] = [:]
+    static var helpers: [String: Helper] = [:]
+    
+    static public func fetchDatabaseData() {
+        
+    }
+}
