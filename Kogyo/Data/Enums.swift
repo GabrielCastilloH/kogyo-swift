333adef1e5956975f3985e3ebe4d4a126c572b37
Gabriel Castillo <gabocastillo2447@icloud.com>
2024-08-01 23:26:32 -0500

    // TODO: 3. Make it so not all the data is loaded when the app is started, only essential data. And all media is loaded once the specific view is loaded. And once its loaded its stored, that way it doesn't have to load again!

---
+++ Kogyo/Data/Enums.swift
@@ -0,0 +1,8 @@
+//
+//  Enums.swift
+//  Kogyo
+//
+//  Created by Gabriel Castillo on 8/1/24.
+//
+
+import Foundation
---
+++ Kogyo/Data/TODO.swift
@@ -0,0 +1,8 @@
+//
+//  TODO.swift
+//  Kogyo
+//
+//  Created by Gabriel Castillo on 8/1/24.
+//
+
+import Foundation
